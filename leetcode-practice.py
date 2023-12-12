30409fd90e46a284e257921db73325dd5abf3ed4
Nicole <nicole.boesenberg@gmail.com>
2023-12-11 19:41:29 -0800

    completed problem 1

--- leetcode-practice.py
+++ leetcode-practice.py
@@ -1,3 +1,20 @@
+# Problem 1
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+# https://leetcode.com/problems/two-sum/description/ 
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+
+
 # Problem 1160
 # https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/description/?envType=daily-question&envId=2023-12-02 
 
